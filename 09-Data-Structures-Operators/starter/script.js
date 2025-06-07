b0183c0978d25d7e73cab46d8e5335ace9f2cb9b
phopha <148693028+phopha@users.noreply.github.com>
2025-06-07 10:16:38 +0700

    finish challenge #4 in 09

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -190,15 +190,55 @@ const gameEvents = new Map([
 ]);
 
 // 1. Create an array 'events' of the different game events that happened (no duplicates)
-const events = [...new Set([...gameEvents.values()])];
-console.log(events);
+// const events = [...new Set([...gameEvents.values()])];
+// console.log(events);
 
 // 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-gameEvents.delete(64);
-console.log(gameEvents);
+// gameEvents.delete(64);
+// console.log(gameEvents);
 
 // 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽️ GOAL
-for(const [k, v] of gameEvents.entries()){
- let r = k < 45 ? "FIRST HALF" : "SECOND HALF";
- console.log(`[${r}] ${k}: ${v}`); 
-}
+// for(const [k, v] of gameEvents.entries()){
+//  let r = k < 45 ? "FIRST HALF" : "SECOND HALF";
+//  console.log(`[${r}] ${k}: ${v}`); 
+// }
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const text = document.querySelector('textarea').value;
+const rows = text.split('\n');
+
+console.log(rows);
