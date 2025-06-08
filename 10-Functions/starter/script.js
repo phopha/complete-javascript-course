9f6eebd35c45f599a61b27e34555f1d95b40868c
phopha <148693028+phopha@users.noreply.github.com>
2025-06-08 19:37:11 +0700

    finish challenge #2 in 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -34,24 +34,48 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  // This generates [0, 0, 0, 0]. More in the next section 😃
-  answers: new Array(4).fill(0),
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
 
-  registerNewAnswer(){
-    console.log(this);
+//   registerNewAnswer(){
+//     console.log(this);
 
-    let answer = Number(prompt(`
-    ${this.question}
-    ${this.options.join('\n')}    
-    `));
-    if(answer < 4 && answer >= 0) this.answers[answer]++;
-  }
-};
+//     let answer = Number(prompt(`
+//     ${this.question}
+//     ${this.options.join('\n')}    
+//     `));
+//     if(answer < 4 && answer >= 0) this.answers[answer]++;
+//   }
+// };
 
-let regis = poll.registerNewAnswer;
+// let regis = poll.registerNewAnswer;
 
-document.querySelector('.poll').addEventListener('click', regis.bind(poll));
+// document.querySelector('.poll').addEventListener('click', regis.bind(poll));
 
+
+
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function(){
+    header.style.color = 'blue';
+    
+  })
+})();
